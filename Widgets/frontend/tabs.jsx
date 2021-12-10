42891862d7a1c4110175b49768ac34838c8ab1cf
calebjo <caleb.ri.jones@gmail.com>
2021-12-10 17:15:09 -0500

    Add code in render in tabs.jsx

--- Widgets/frontend/tabs.jsx
+++ Widgets/frontend/tabs.jsx
@@ -8,23 +8,31 @@ class Tabs extends React.Component {
         // this.tabs = [ { 'tab1': "first tab" },{ 'tab2': "second tab"  },{ 'tab3': "third tab" } ]
         this.titles = ['tab1', 'tab2', 'tab3']
         this.content = ['first content', 'second content', 'third content']
+        this.updateContent = this.updateContent.bind(this)
+    }
+
+    updateContent(e){
+        // console.log(e.target.innerText)
+        const title = e.target.innerText
+        const contentIdx = this.titles.indexOf(title)
+        this.setState({
+            content: this.content[contentIdx]
+        })
     }
 
     render(){
-
-
         return(
             <div>
                 <ul>
                     {this.titles.map((title,idx) => {
-                        return <li onClick={updateContent} key={idx}>{title}</li>})
+                        return <li onClick={this.updateContent} key={idx}>{title}</li>})
                     }
                 </ul>
                     < article >
                         { this.content }
                     </article>
             </div>
-        )
+        );
     }
 }
 
--- Widgets/widgets.jsx
+++ Widgets/widgets.jsx
@@ -5,7 +5,7 @@ import Tabs from './frontend/tabs';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById("root");
-    ReactDOM.render(<Clock/>, root);
+    // ReactDOM.render(<Clock/>, root);
     ReactDOM.render(<Tabs/>, root);
 });
 
